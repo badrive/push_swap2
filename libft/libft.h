3df418e3a690f4e7aeffa72515fa65ca7199121b
badrive <badrive@example.com>
2025-01-24 18:54:22 +0100

    remove lst_iter

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bfaras <bfaras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:11:09 by bfaras            #+#    #+#             */
-/*   Updated: 2025/01/24 18:49:35 by bfaras           ###   ########.fr       */
+/*   Updated: 2025/01/24 18:53:56 by bfaras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,6 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 // void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst);
-void				ft_lstiter(t_list *lst, void (*f)(void *));
+// void				ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
