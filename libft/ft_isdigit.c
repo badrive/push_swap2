5059e96c67f87bf0b83a6164a3614a294150f60b
badrive <badirov.faras@gmail.com>
2025-02-25 20:11:39 +0000

    fix double -- & ++

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: bfaras <bfaras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:17:30 by bfaras            #+#    #+#             */
-/*   Updated: 2025/02/24 16:57:17 by bfaras           ###   ########.fr       */
+/*   Updated: 2025/02/25 20:09:49 by bfaras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,11 @@ int	ft_isdigit(char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] == '-')
-		i++;
 	while (str[i] != '\0')
 	{
 		if (str[i] < '0' || str[i] > '9')
 			return (0);
 		i++;
 	}
-	if (i == 0 || (i == 1 && str[0] == '-'))
-		return (0);
 	return (1);
 }
